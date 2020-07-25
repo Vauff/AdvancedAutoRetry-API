0a37b5ac32fac71aa38b1b014aba7e989f46c197
Vauff <mctehkitti@gmail.com>
2020-07-24 23:17:10 -0400

    Catch exceptions from HTTP timeout & fix formatting

--- Exceptions/CloudflareException.cs
+++ Exceptions/CloudflareException.cs
@@ -7,7 +7,7 @@ namespace AdvancedAutoRetry_API.Exceptions
 {
 	public class CloudflareException : Exception
 	{
-        public CloudflareException()
+		public CloudflareException()
 		{
 		}
 
--- FirewallEventConsumer.cs
+++ FirewallEventConsumer.cs
@@ -24,57 +24,57 @@ namespace AdvancedAutoRetry_API
 			_client = client;
 		}
 
-        public async Task<List<FirewallEvent>> GetFirewallEvents(string clientIP)
-        {
-            DateTime now = DateTime.UtcNow;
-            int attempts = 0;
-            bool commFail = false;
-            bool apiError = false;
+		public async Task<List<FirewallEvent>> GetFirewallEvents(string clientIP)
+		{
+			DateTime now = DateTime.UtcNow;
+			int attempts = 0;
+			bool commFail = false;
+			bool apiError = false;
 
-            if (Program.Debug)
-                Console.WriteLine("geq: " + now.AddMinutes(Program.FirewallEventMinutes * -1).ToString("o") + " leq: " + now.AddMinutes(1).ToString("o"));
+			if (Program.Debug)
+				Console.WriteLine("geq: " + now.AddMinutes(Program.FirewallEventMinutes * -1).ToString("o") + " leq: " + now.AddMinutes(1).ToString("o"));
 
-            while (attempts < 4)
-            {
-                try
-                {
-                    var query = new GraphQLRequest
-                    {
-                        Query = "query { viewer { zones(filter: {zoneTag: \"" + Program.ZoneID + "\"}) { firewallEventsAdaptive(filter: { datetime_geq: \"" + now.AddMinutes(Program.FirewallEventMinutes * -1).ToString("o") + "\", datetime_leq: \"" + now.AddMinutes(1).ToString("o") + "\", clientIP: \"" + clientIP + "\" }, limit: 10000, orderBy: [datetime_DESC]) { clientIP clientRequestPath }}}}"
-                    };
-
-                    var response = await _client.SendQueryAsync<ResponseData>(query);
-
-                    if (response.Errors != null)
+			while (attempts < 4)
+			{
+				try
+				{
+					var query = new GraphQLRequest
 					{
-                        if (Program.Debug)
+						Query = "query { viewer { zones(filter: {zoneTag: \"" + Program.ZoneID + "\"}) { firewallEventsAdaptive(filter: { datetime_geq: \"" + now.AddMinutes(Program.FirewallEventMinutes * -1).ToString("o") + "\", datetime_leq: \"" + now.AddMinutes(1).ToString("o") + "\", clientIP: \"" + clientIP + "\" }, limit: 10000, orderBy: [datetime_DESC]) { clientIP clientRequestPath }}}}"
+					};
+
+					var response = await _client.SendQueryAsync<ResponseData>(query);
+
+					if (response.Errors != null)
+					{
+						if (Program.Debug)
 						{
-                            foreach (GraphQLError error in response.Errors)
-                                Console.WriteLine("API error: " + error.Message);
+							foreach (GraphQLError error in response.Errors)
+								Console.WriteLine("API error: " + error.Message);
 						}
-                        
-                        apiError = true;
-                        attempts++;
-                        Thread.Sleep(2000);
-                        continue;
+						
+						apiError = true;
+						attempts++;
+						Thread.Sleep(2000);
+						continue;
 					}
 
-                    return response.Data.viewer.zones[0].firewallEventsAdaptive;
-                }
-                catch (Exception e) when (e is HttpRequestException || e is GraphQLHttpRequestException)
-                {
-                    commFail = true;
-                    attempts++;
-                    Thread.Sleep(2000);
-                }
-            }
+					return response.Data.viewer.zones[0].firewallEventsAdaptive;
+				}
+				catch (Exception e) when (e is HttpRequestException || e is GraphQLHttpRequestException || e is TaskCanceledException)
+				{
+					commFail = true;
+					attempts++;
+					Thread.Sleep(2000);
+				}
+			}
 
-            if (commFail && apiError)
-                throw new CloudflareException("Multiple Cloudflare API errors: Errors were returned and communication failed, see debug info");
-            else if (apiError)
-                throw new CloudflareException("The Cloudflare API returned an error, see debug info");
-            else
-                throw new CloudflareException("Communication with the Cloudflare API failed");
-        }
-    }
+			if (commFail && apiError)
+				throw new CloudflareException("Multiple Cloudflare API errors: Errors were returned and communication failed, see debug info");
+			else if (apiError)
+				throw new CloudflareException("The Cloudflare API returned an error, see debug info");
+			else
+				throw new CloudflareException("Communication with the Cloudflare API failed");
+		}
+	}
 }
